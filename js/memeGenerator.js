1dc5f9f013c6e694fec8ffae513650d237b6253e
carolinass <carolzinhacabral@gmail.com>
2016-12-29 02:57:27 -0300

    Upload image and bottom text input working.

--- js/memeGenerator.js
+++ js/memeGenerator.js
@@ -16,6 +16,28 @@ $( document ).ready( function() {
 	c.height = 500;
 	c.width = 500;
     var topText = "";
+    var bottomText = "";
+
+    $( "#fileImage" ).change(function(){
+    	console.log($( "#fileImage" ))
+
+    	var file    = document.querySelector('input[type=file]').files[0]; //sames as here
+		var reader  = new FileReader();
+
+       	reader.onloadend = function () {
+       		imageObj = new Image();
+			imageObj.onload = function() {
+				imageCtx.drawImage(imageObj, 0, 0, 500, 500);
+			};
+			imageObj.src = reader.result;
+       	}
+
+  		if (file) {
+           reader.readAsDataURL(file); //reads the data as a URL
+       	} else {
+           imageObj.src = "";
+       	}
+    });
 
 	$( "#loadBtn" ).click(function(){
 		console.log("clique");
@@ -23,18 +45,35 @@ $( document ).ready( function() {
 
 	$( "#top-text" ).keyup(function(evt){
 		topText = $( "#top-text" ).val();
-		ctx.font = "36pt Impact";
+		ctx.font = "bold 36pt Impact";
 		ctx.textAlign = "center";
-		redrawTexts(topText);
+		redrawTopText(topText);
 	});
 
-	function redrawTexts(topText){
-        ctx.clearRect(0,0,c.width,c.height);   
+	$( "#bottom-text" ).keyup(function(evt){
+		bottomText = $( "#bottom-text" ).val();
+		ctx.font = "bold 36pt Impact";
+		ctx.textAlign = "center";
+		redrawBottomText(bottomText);
+	});
+
+	function redrawTopText(topText, bottomText){
+        ctx.clearRect(0,0,c.width,c.height-50);   
         ctx.strokeStyle = "black";
 		ctx.lineWidth = 3;
 		ctx.strokeText(topText, c.width/2, 60);
 		ctx.fillStyle = "white"
 		ctx.fillText(topText, c.width/2, 60);
+    }
+
+    function redrawBottomText(bottomText) {
+    	ctx.clearRect(0,100,c.width-100,c.height);   
+    	ctx.strokeStyle = "black";
+		ctx.lineWidth = 3;
+		ctx.strokeText(bottomText, c.width/2, c.height - 20);
+		ctx.fillStyle = "white"
+		ctx.fillText(bottomText, c.width/2, c.height - 20);
+
     }
 
 });
